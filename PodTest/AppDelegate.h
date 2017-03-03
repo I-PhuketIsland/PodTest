b20d9753c2613197d28fa32f331661670255b033
ysz_lee <347456222@qq.com>
2017-03-03 15:51:30 +0800

    测试忽略文件

---
+++ PodTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodTest
+//
+//  Created by Lee on 2017/3/3.
+//  Copyright © 2017年 李家乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PodTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PodTest
+//
+//  Created by Lee on 2017/3/3.
+//  Copyright © 2017年 李家乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
